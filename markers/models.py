15167662758b4b681ab198a51e31bbbbeff67a80
Rodrigo <rodhoff@gmail.com>
2024-02-25 19:16:48 -0300

    First Successful Migration to PostgreSQL

--- markers/models.py
+++ markers/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Marker(models.Model):
+    name = models.CharField(
+        max_length=255
+    )
+    location = models.PointField()
+
+    def __str__(self):
+        return self.name
--- mymap/settings.py
+++ mymap/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mymap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.contrib.gis.db.backends.postgis",
+        "HOST": "localhost",
+        "NAME": "postgres",
+        "PASSWORD": "@Brasil01",
+        "PORT": 5432,
+        "USER": "postgres"
     }
 }
 
